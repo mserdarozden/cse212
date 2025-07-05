3dec1549bd24b70a3b7aa08330f712a88cb963e3
mserdarozden <mserdarozden@me.com>
2025-07-05 14:14:43 +0300

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,20 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create an array to hold the results
+        var results = new double[length];
 
-        return []; // replace this return statement with your own
+        // Initialize an index to keep track of the current position in the array
+        var index = 0;
+
+        // Loop from 1 to length, multiplying the number by the loop variable
+        for (var i = 1; i <= length; i++)
+        {
+            results[index++] = number * i; // Store the multiple in the array
+        }
+
+        // Return the array of multiples
+        return results;
     }
 
     /// <summary>
@@ -25,9 +33,22 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Find the index where the rotation should start
+        var index = data.Count - amount;
+
+        // Get the last 'amount' elements that will be moved to the front
+        List<int> lastPart = data.GetRange(index, amount);
+
+        // Get the first part of the list that will be moved after the rotated part
+        List<int> firstPart = data.GetRange(0, index);
+
+        // Combine the rotated part with the remaining part
+        lastPart.AddRange(firstPart);
+
+        // Clear the original list
+        data.Clear();
+
+        // Add the rotated elements back to the original list
+        data.AddRange(lastPart);
     }
 }
